9e14de76bb6d9c2d88242c74e3ca3136adf9f96f
ashwinak <ashwinak@umich.edu>
2024-06-08 23:02:33 -0400

    Increased mines to 10

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Board from "@/components/board";
 export default function Home() {
   const rows = 8;
   const cols = 8;
-  const mines = 2;
+  const mines = 10;
 
   return (
     // <main className="flex flex-row min-h-screen justify-center items-center">
